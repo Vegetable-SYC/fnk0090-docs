e62514670bc085f29e966909cc6bd420e2f74a68
Eeason Shen <109536807+Vegetable-SYC@users.noreply.github.com>
2025-01-15 17:47:45 +0800

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,8 @@ import sys
 
 sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
 
-# os.system("rm -r freenove_Kit")
-# os.system("git clone --depth 1 https://github.com/Freenove/Freenove_ESP32_WROOM_Board freenove_Kit")
+os.system("rm -r freenove_Kit")
+os.system("git clone --depth 1 https://github.com/Freenove/Freenove_ESP32_WROOM_Board freenove_Kit")
 
 
 def prepend_to_file(file_path, content):
